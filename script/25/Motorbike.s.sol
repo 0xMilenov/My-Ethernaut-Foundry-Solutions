9407b0c9507ad5ab658c4e162c45568f469ba343
0xMilenov <137901924+0xMilenov@users.noreply.github.com>
2023-10-01 11:24:33 +0300

    Motorbike and DoubleEntryPoint solved

---
+++ script/25/Motorbike.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// 0xMilenov's solution to ethernaut
+
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import {Motorbike} from "src/25/Motorbike.sol";
+
+contract Exploit is Script {
+    address payable instance = payable(vm.envAddress("INSTANCE_LEVEL25"));
+    address hacker = vm.rememberKey(vm.envUint("PRIVATE_KEY"));
+    Motorbike level = Motorbike(instance);
+
+    function run() external {
+        vm.startBroadcast(hacker);
+
+        vm.stopBroadcast();
+    }
+}
---
+++ script/26/DoublyEntryPoint.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// 0xMilenov's solution to ethernaut
+
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import {DoublyEntryPoint} from "src/26/DoubleEntryPoint.sol";
+
+contract Exploit is Script {
+    address instance = vm.envAddress("INSTANCE_LEVEL26");
+    address hacker = vm.rememberKey(vm.envUint("PRIVATE_KEY"));
+    DoublyEntryPoint level = DoublyEntryPoint(instance);
+
+    function run() external {
+        vm.startBroadcast(hacker);
+
+        vm.stopBroadcast();
+    }
+}
