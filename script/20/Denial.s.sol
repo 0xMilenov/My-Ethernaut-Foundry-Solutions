d7c7d446796dd48faa1287cf10722bd25e52f863
0xMilenov <137901924+0xMilenov@users.noreply.github.com>
2023-10-01 11:12:19 +0300

    Denial, Shop and DexOne solved

---
+++ script/20/Denial.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// 0xMilenov's solution to ethernaut
+
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import {Denial} from "src/20/Denial.sol";
+
+contract Exploit is Script {
+    address instance = vm.envAddress("INSTANCE_LEVEL20");
+    address hacker = vm.rememberKey(vm.envUint("PRIVATE_KEY"));
+    Denial level = Denial(instance);
+
+    function run() external {
+        vm.startBroadcast(hacker);
+
+        vm.stopBroadcast();
+    }
+}
