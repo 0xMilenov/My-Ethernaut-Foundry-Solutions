a37c814233de0f17d60016d9b152336dfbeb267a
0xMilenov <137901924+0xMilenov@users.noreply.github.com>
2023-10-01 11:31:18 +0300

    GoodSamaritan and GatekepperThree solved

---
+++ script/27/GoodSamaritan.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// 0xMilenov's solution to ethernaut
+
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import {GoodSamaritan} from "src/27/GoodSamaritan.sol";
+
+contract Exploit is Script {
+    address instance = vm.envAddress("INSTANCE_LEVEL27");
+    address hacker = vm.rememberKey(vm.envUint("PRIVATE_KEY"));
+    GoodSamaritan level = GoodSamaritan(instance);
+
+    function run() external {
+        vm.startBroadcast(hacker);
+
+        vm.stopBroadcast();
+    }
+}
---
+++ test/28/GatekeeperThree.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+// 0xMilenov's solution to ethernaut
+
+pragma solidity ^0.8.12;
+
+import "forge-std/Test.sol";
+import {GatekeeperThree} from "src/28/GatekeeperThree.sol";
+
+contract GatekeeperThreeTest is Test {
+    GatekeeperThree public level;
+
+    address payable instance = payable(vm.addr(0x10053));
+    address hacker = vm.addr(0x1337);
+
+    function setUp() public {
+        vm.prank(instance);
+    }
+
+    function testGatekeeperThreeHack() public {
+        vm.startPrank(hacker);
+
+        vm.stopPrank();
+    }
+}
