5de06040b75d3116d44de10ed1135c4596a78a4c
0xMilenov <137901924+0xMilenov@users.noreply.github.com>
2023-10-01 10:48:58 +0300

    GatekeeperTwo, NaughtCoin and Preservation solved

---
+++ script/14/GatekeeperTwo.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// 0xMilenov's solution to ethernaut
+
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import {GatekeeperTwo} from "src/14/GatekeeperTwo.sol";
+
+contract Exploit is Script {
+    address instance = vm.envAddress("INSTANCE_LEVEL14");
+    address hacker = vm.rememberKey(vm.envUint("PRIVATE_KEY"));
+    GatekeeperTwo level = GatekeeperTwo(instance);
+
+    function run() external {
+        vm.startBroadcast(hacker);
+
+        vm.stopBroadcast();
+    }
+}
---
+++ script/16/Preservation.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// 0xMilenov's solution to ethernaut
+
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import {Preservation} from "src/16/Preservation.sol";
+
+contract Exploit is Script {
+    address instance = vm.envAddress("INSTANCE_LEVEL16");
+    address hacker = vm.rememberKey(vm.envUint("PRIVATE_KEY"));
+    Preservation level = Preservation(instance);
+
+    function run() external {
+        vm.startBroadcast(hacker);
+
+        vm.stopBroadcast();
+    }
+}
