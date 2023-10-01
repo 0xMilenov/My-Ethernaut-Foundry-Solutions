bc35c60fb16128e8de86356bf98dd96ff9f2cd26
0xMilenov <137901924+0xMilenov@users.noreply.github.com>
2023-10-01 08:00:24 +0300

    Solved Reentrancy and Elevator challenges

---
+++ script/11/Elevator.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+// 0xMilenov's solution to ethernaut
+
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import {Elevator} from "src/11/Elevator.sol";
+import {ElevatorExploit} from "src/11/ElevatorExploit.sol";
+
+contract Exploit is Script {
+    address instance = vm.envAddress("INSTANCE_LEVEL11");
+    address hacker = vm.rememberKey(vm.envUint("PRIVATE_KEY"));
+    Elevator level = Elevator(instance);
+    ElevatorExploit public exploit;
+
+    function run() external {
+        vm.startBroadcast(hacker);
+
+        exploit = new ElevatorExploit();
+        exploit.run(level);
+
+        vm.stopBroadcast();
+    }
+}
