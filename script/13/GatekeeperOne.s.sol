59961fe2842595b81865d5194c61de1472c5fa53
0xMilenov <137901924+0xMilenov@users.noreply.github.com>
2023-10-01 10:43:32 +0300

    Solved Privacy and GateKeeperOne

---
+++ script/13/GatekeeperOne.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// 0xMilenov's solution to ethernaut
+
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import {GatekeeperOne} from "src/13/GatekeeperOne.sol";
+
+contract Exploit is Script {
+    address instance = vm.envAddress("INSTANCE_LEVEL13");
+    address hacker = vm.rememberKey(vm.envUint("PRIVATE_KEY"));
+    GatekeeperOne level = GatekeeperOne(instance);
+
+    function run() external {
+        vm.startBroadcast(hacker);
+
+        vm.stopBroadcast();
+    }
+}
---
+++ test/13/GatekeeperOne.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+// 0xMilenov's solution to ethernaut
+
+pragma solidity ^0.8.12;
+
+import "forge-std/Test.sol";
+import {GatekeeperOne} from "src/13/GatekeeperOne.sol";
+
+contract GatekeeperOneTest is Test {
+    GatekeeperOne public level;
+
+    address payable instance = payable(vm.addr(0x10053));
+    address hacker = vm.addr(0x1337);
+
+    function setUp() public {
+        vm.prank(instance);
+    }
+
+    function testPrivacyHack() public {
+        vm.startPrank(hacker);
+
+        vm.stopPrank();
+    }
+}
