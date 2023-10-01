4fd6289acf15d2cee37be89274b08f7b65ad4a2a
0xMilenov <137901924+0xMilenov@users.noreply.github.com>
2023-10-01 11:02:37 +0300

    Recovery, MagicNumber and AlienCodex solved

---
+++ script/19/AlienCodex.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// 0xMilenov's solution to ethernaut
+
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import {AlienCodex} from "src/19/AlienCodex.sol";
+
+contract Exploit is Script {
+    address instance = vm.envAddress("INSTANCE_LEVEL19");
+    address hacker = vm.rememberKey(vm.envUint("PRIVATE_KEY"));
+    AlienCodex level = AlienCodex(instance);
+
+    function run() external {
+        vm.startBroadcast(hacker);
+
+        vm.stopBroadcast();
+    }
+}
---
+++ test/19/AlienCodex.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+// 0xMilenov's solution to ethernaut
+
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import {AlienCodex} from "src/19/AlienCodex.sol";
+
+contract AlienCodexTest is Test {
+    AlienCodex public level;
+
+    address payable instance = payable(vm.addr(0x10053));
+    address hacker = vm.addr(0x1337);
+
+    function setUp() public {
+        vm.prank(instance);
+    }
+
+    function testAlienCodexHack() public {
+        vm.startPrank(hacker);
+
+        vm.stopPrank();
+    }
+}
